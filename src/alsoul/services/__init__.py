74dac7e7444b2e2475e350ad10cd206a55c2836d
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-06 22:09:23 +0300

    Export provider recovery coordinator

--- src/alsoul/services/__init__.py
+++ src/alsoul/services/__init__.py
@@ -1,10 +1,12 @@
 from .bootstrap import FoundationBootstrapper
 from .foundation import FoundationServices
 from .provider_integration import ModelGenerationRunner, WorldAcquisitionRunner
+from .provider_recovery import ProviderRecoveryCoordinator
 
 __all__ = [
     "FoundationBootstrapper",
     "FoundationServices",
     "ModelGenerationRunner",
+    "ProviderRecoveryCoordinator",
     "WorldAcquisitionRunner",
 ]
