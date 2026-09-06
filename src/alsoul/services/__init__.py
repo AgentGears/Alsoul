400bb5b4621386480af2bf09aa07cb5ee7ff4a0f
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-06 19:47:23 +0300

    Export provider integration runners

--- src/alsoul/services/__init__.py
+++ src/alsoul/services/__init__.py
@@ -1,4 +1,10 @@
 from .bootstrap import FoundationBootstrapper
 from .foundation import FoundationServices
+from .provider_integration import ModelGenerationRunner, WorldAcquisitionRunner
 
-__all__ = ["FoundationBootstrapper", "FoundationServices"]
+__all__ = [
+    "FoundationBootstrapper",
+    "FoundationServices",
+    "ModelGenerationRunner",
+    "WorldAcquisitionRunner",
+]
