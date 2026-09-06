22de0dd62abbf48f653ca51055c1a4ef299e5de2
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-06 19:46:42 +0300

    Export controlled provider adapters

--- src/alsoul/adapters/__init__.py
+++ src/alsoul/adapters/__init__.py
@@ -1,14 +1,33 @@
-from .contracts import ModelProviderAdapter, WorldAcquisitionAdapter
+from .contracts import (
+    AdapterError,
+    AdapterOutcomeUnknown,
+    AdapterRejected,
+    ModelProviderAdapter,
+    WorldAcquisitionAdapter,
+)
 from .fakes import FakeModelAdapter, FakeWorldAdapter
 from .http_world import HttpResponse, HttpTransport, HttpWorldAdapter, UrllibHttpTransport
+from .json_model import (
+    JsonHttpResponse,
+    JsonHttpTransport,
+    JsonModelProviderAdapter,
+    UrllibJsonTransport,
+)
 
 __all__ = [
+    "AdapterError",
+    "AdapterOutcomeUnknown",
+    "AdapterRejected",
     "FakeModelAdapter",
     "FakeWorldAdapter",
     "HttpResponse",
     "HttpTransport",
     "HttpWorldAdapter",
+    "JsonHttpResponse",
+    "JsonHttpTransport",
+    "JsonModelProviderAdapter",
     "ModelProviderAdapter",
     "UrllibHttpTransport",
+    "UrllibJsonTransport",
     "WorldAcquisitionAdapter",
 ]
