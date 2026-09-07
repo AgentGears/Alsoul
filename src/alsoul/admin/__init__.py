d4cf9e981f36c5d013a817f93a2822f72f34002f
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-07 05:05:45 +0300

    Export foundation administration boundary

---
+++ src/alsoul/admin/__init__.py
@@ -0,0 +1,15 @@
+from .service import (
+    AdministrationError,
+    FoundationAdministrator,
+    InitializeStoreResult,
+    MigrateStoreResult,
+    StoreStatus,
+)
+
+__all__ = [
+    "AdministrationError",
+    "FoundationAdministrator",
+    "InitializeStoreResult",
+    "MigrateStoreResult",
+    "StoreStatus",
+]
